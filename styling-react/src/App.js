154e31d08594ab7197efa3b5b02bcc016fff6483
사공지은 <gonii0821@naver.com>
2022-10-04 01:09:27 +0900

    practice SASS 💅

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,5 +1,10 @@
-function App() {
-  return <div></div>;
+import React, { Component } from "react";
+import SassComponent from "./SassComponent";
+
+class App extends Component {
+  render() {
+    return <SassComponent />;
+  }
 }
 
 export default App;
