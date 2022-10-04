ba06fd76abc7e1f2473755bef896a76a05f6b4e9
사공지은 <gonii0821@naver.com>
2022-10-04 23:31:25 +0900

    practice styled-components 💅

---
+++ styling-react/src/StyledComponent.js
@@ -0,0 +1,73 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const sizes = {
+  desktop: 1024,
+  tablet: 768,
+};
+
+// styled-components 매뉴얼에서 제공하는 유틸 함수를 사용하여 반응형 media 쿼리 함수 만들기
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `;
+});
+
+const Box = styled.div`
+  background: ${(props) => props.color || "blue"};
+  padding: 1rem;
+  display: flex;
+  width: 1024px;
+  margin: 0 auto;
+
+  ${media.desktop`width: 768px;`}
+  ${media.tablet`width: 100%;`}
+`;
+
+const Button = styled.button`
+  background: white;
+  color: black;
+  border-radius: 4px;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  font-size: 1rem;
+  font-weight: 600;
+
+  // & 문자를 사용해 Sass처럼 자기 자신 선택 가능
+  &:hover {
+    background: rgba(255, 255, 255, 0.9);
+  }
+
+  ${(props) =>
+    props.inverted &&
+    css`
+      background: none;
+      border: 2px solid white;
+      color: white;
+
+      &:hover {
+        background: white;
+        color: black;
+      }
+    `};
+
+  & + button {
+    margin-left: 1rem;
+  }
+`;
+
+const StyledComponent = () => {
+  return (
+    <Box color="black">
+      <Button>안녕하세요</Button>
+      <Button inverted={true}>테두리만</Button>
+    </Box>
+  );
+};
+
+export default StyledComponent;
