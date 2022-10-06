2d5c18cb50cec57ae6c3d70c505d3ee0a120d579
사공지은 <gonii0821@naver.com>
2022-10-06 23:51:50 +0900

    maintenance of immutability using immer library 💡

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,7 +1,89 @@
-import "./App.css";
+import { useRef, useCallback, useState } from "react";
+import produce from "immer";
 
 function App() {
-  return <div></div>;
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: "", username: "" });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+
+  // input 수정을 위한 함수
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm(
+      produce((draft) => {
+        draft[name] = value;
+      })
+    );
+  }, []);
+
+  // form 등록을 위한 함수
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      setData(
+        produce((draft) => {
+          draft.array.push(info);
+        })
+      );
+
+      setForm({
+        name: "",
+        username: "",
+      });
+    },
+    [form.name, form.username]
+  );
+
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback((id) => {
+    setData(
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1
+        );
+      })
+    );
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="아이디"
+          value={form.username}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="이름"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
