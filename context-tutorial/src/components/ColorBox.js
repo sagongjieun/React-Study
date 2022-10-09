224884c9bdd98ba6448d86ad2a9d4d16efd3a13a
사공지은 <gonii0821@naver.com>
2022-10-10 03:04:50 +0900

    practice useContext ✍

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,33 +1,28 @@
-import colorContext from "../contexts/color";
 import { ColorConsumer } from "../contexts/color";
+import { useContext } from "react";
+import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
+
   return (
-    <ColorConsumer>
-      {(value) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: value.state.color,
-            }}
-          ></div>
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: value.state.subcolor,
-            }}
-          ></div>
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      ></div>
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      ></div>
+    </>
   );
 };
-
-// Consumer 사이에 중괄호를 열어서 함수를 넣음
-// -> Function as a child
-// -> Render Props
 
 export default ColorBox;
